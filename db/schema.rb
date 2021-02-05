00c3e2e7f4c0aa241a7a8f7fa1095b5ff4eba97b
Tsubasa <aile104fujikawa@gmail.com>
2021-02-05 10:31:58 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_132905) do
+ActiveRecord::Schema.define(version: 2021_01_27_132401) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
